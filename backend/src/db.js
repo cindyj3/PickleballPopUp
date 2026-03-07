2a3542f88f743eecd17a6b89face022ce3c72a5e
Cindy Jiang <124411897+cindyj3@users.noreply.github.com>
2026-03-07 12:30:34 -0600

    initialize database schema on startup

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,9 +1,15 @@
-const path = require("path");
 const Database = require("better-sqlite3");
+const fs = require("fs");
+const path = require("path");
 
-const dbPath = path.join(__dirname, "..", "db", "pickleball.db");
+const dbPath = path.join(__dirname, "../../db/pickleball.db");
 const db = new Database(dbPath);
 
-db.pragma("foreign_keys = ON");
+const schemaPath = path.join(__dirname, "../../db/schema.sql");
+
+if (fs.existsSync(schemaPath)) {
+  const schema = fs.readFileSync(schemaPath, "utf8");
+  db.exec(schema);
+}
 
 module.exports = db;
